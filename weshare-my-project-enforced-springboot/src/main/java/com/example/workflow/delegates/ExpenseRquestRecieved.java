0e101c266bf33909ca522ef91791fa44f5ca860d
Sibusiso Magwaxaza <F5625076@FNB.CO.ZA>
2023-02-27 13:39:55 +0200

    commit13

--- weshare-my-project-enforced-springboot/src/main/java/com/example/workflow/delegates/ExpenseRquestRecieved.java
+++ weshare-my-project-enforced-springboot/src/main/java/com/example/workflow/delegates/ExpenseRquestRecieved.java
@@ -5,12 +5,14 @@ import com.example.workflow.valueobject.Expense;
 import com.example.workflow.valueobject.Person;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class ExpenseRquestRecieved implements JavaDelegate {
     private final ExpenseService expenseService;
-
+    @Autowired
     public ExpenseRquestRecieved(ExpenseService expenseService) {
         this.expenseService = expenseService;
     }
